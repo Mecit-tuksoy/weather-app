f3d873922a1645de6ba4deeb1f2b12a6db689f2a
Mecit-tuksoy <mecit.tuksoy@gmail.com>
2025-06-21 19:14:59 +0300

    url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const API_BASE = process.env.REACT_APP_API_URL || "";
 
 export const getWeatherLogs = async () => {
   const res = await axios.get(`${API_BASE}/weather-logs`);
-console.log(res.data);
+  console.log(res.data);
 
   return res.data.data;
 };
